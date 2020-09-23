4ba8568c635ef57ba110b7aaad7bfcc14a934677
Alexander Raish <leidenn2509@gmail.com>
2020-09-23 23:58:46 +0700

    Write my map and filter

--- src/tasks/utils.clj
+++ src/tasks/utils.clj
@@ -1,4 +1,16 @@
 (ns tasks.utils)
 
+
+(defn my-map [f coll]
+    (apply list (reduce
+                    (fn [coll x] (conj coll (f x)))
+                    []
+                    coll)))
+
+(defn my-filter [pred coll]
+    (apply list (reduce (fn [coll x] (if (pred x) (conj coll x) coll))
+                        []
+                        coll)))
+
 (defn first-char [string]
     (.substring string 0 1))
