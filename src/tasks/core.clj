6ad6816666a60a7b6e9aa153c7cdb9f6432b18fb
Alexander Raish <leidenn2509@gmail.com>
2020-09-24 00:37:52 +0700

    Write task14

--- src/tasks/core.clj
+++ src/tasks/core.clj
@@ -1,7 +1,8 @@
 (ns tasks.core
     (:use tasks.utils)
     (:use tasks.task11)
-    (:use tasks.task12))
+    (:use tasks.task12)
+    (:use tasks.task14))
 
 (defn -main
     [])
--- test/tasks/core_test.clj
+++ test/tasks/core_test.clj
@@ -22,3 +22,11 @@
         (is (= (tasks.task12/task12 `("a" "b" "c") 2) `("ac" "ab" "bc" "ba" "cb" "ca")))
         (is (= (tasks.task12/task12 `("a" "b" "c") 3) `("aca" "acb" "aba" "abc" "bca" "bcb" "bab" "bac" "cba" "cbc" "cab" "cac")))
         (is (= (tasks.task12/task12 `() 1) `()))))
+
+(deftest task14-test
+    (testing "task11"
+        (is (= (tasks.task14/task14 `("a" "b" "c") 0) `()))
+        (is (= (tasks.task14/task14 `("a" "b" "c") 1) `("a" "b" "c")))
+        (is (= (tasks.task14/task14 `("a" "b" "c") 2) `("ab" "ac" "ba" "bc" "ca" "cb")))
+        (is (= (tasks.task14/task14 `("a" "b" "c") 3) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))
+        (is (= (tasks.task14/task14 `() 1) `()))))
