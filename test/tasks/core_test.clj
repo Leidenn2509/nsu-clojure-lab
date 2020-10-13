4ac483e072b01d15f33c9c202c6b247a569d7523
Alexander Raish <leidenn2509@gmail.com>
2020-10-13 18:24:46 +0700

    fix tests

--- test/tasks/core_test.clj
+++ test/tasks/core_test.clj
@@ -3,18 +3,6 @@
             [tasks.core :refer :all]))
 
 
-(deftest task11-test
-  (testing "task11"
-      (is (= (tasks.task11/task11 `("a" "b" "c") 0) `()))
-      (is (= (tasks.task11/task11 `("a" "b" "c") 1) `("a" "b" "c")))
-      (is (= (tasks.task11/task11 `("a" "b" "c") 2) `("ab" "ac" "ba" "bc" "ca" "cb")))
-      (is (= (tasks.task11/task11 `("a" "b" "c") 3) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))
-      (is (= (tasks.task11/task11 `() 1) `()))))
-      ;(is (thrown? StackOverflowError (tasks.task11 `("a" "b" "c") 15)))
-
-
-
-
 (deftest task12-test
     (testing "task12"
         (is (= (tasks.task12/task12 `("a" "b" "c") 0) `()))
@@ -22,6 +10,14 @@
         (is (= (tasks.task12/task12 `("a" "b" "c") 2) `("ac" "ab" "bc" "ba" "cb" "ca")))
         (is (= (tasks.task12/task12 `("a" "b" "c") 3) `("aca" "acb" "aba" "abc" "bca" "bcb" "bab" "bac" "cba" "cbc" "cab" "cac")))
         (is (= (tasks.task12/task12 `() 1) `()))))
+
+(deftest task11-test
+    (testing "task11"
+        (is (= (tasks.task11/task11 `("a" "b" "c") 0) `()))
+        (is (= (tasks.task11/task11 `("a" "b" "c") 1) `("a" "b" "c")))
+        (is (= (tasks.task11/task11 `("a" "b" "c") 2) `("ab" "ac" "ba" "bc" "ca" "cb")))
+        (is (= (tasks.task11/task11 `("a" "b" "c") 3) `("aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc")))
+        (is (= (tasks.task11/task11 `() 1) `()))))
 
 (deftest task14-test
     (testing "task11"
